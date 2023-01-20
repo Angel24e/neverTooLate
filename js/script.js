62100314c84876f042162a95dbaf54534caab476
angel Escamilla <angel4esc@gmail.com>
2023-01-19 17:30:47 -0700

    Finishing up basic design and adding tiny bit of functionality to the search button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 const breweryApi = `https://api.openbrewerydb.org/breweries`
 
-fetch(breweryApi)
+const searchButton = document.querySelector(".handleSearchButton");
+
+const searchInput = document.getElementById("brewery")
+
+function searchForBrewery() {
+  event.preventDefault();
+  console.log(searchInput.value)
+  fetch(breweryApi)
   .then(function (response) {
     return response.json();
   })
   .then(function(data) {
     console.log(data);
+    displayBreweryLocation(data);
   });
+}
+
+function displayBreweryLocation(data) {
+  console.log("Current Brewery Data: ", data);
+}
+
+searchButton.addEventListener("click", searchForBrewery)
